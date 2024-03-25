94e7aaa789930b798c6cd4381e6bbf69af0de380
loujain diab <loujaindiab551@gmail.com>
2024-03-25 13:24:21 +0300

    init project

---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Traits;
+
+trait HttpResponses
+{
+    protected function succes($data,$message=null,$status_code=200): \Illuminate\Http\JsonResponse
+    {
+
+        return response()->json(
+            [
+                'status'=>'Request was succesful',
+                'message'=>$message,
+                'data'=>$data
+            ],$status_code);
+    }
+
+    protected function wrong($data,$message=null,$status_code): \Illuminate\Http\JsonResponse
+    {
+
+        return response()->json(
+            [
+                'status'=>'An error has occurred',
+                'message'=>$message,
+                'data'=>$data
+            ],$status_code);
+    }
+
+}
