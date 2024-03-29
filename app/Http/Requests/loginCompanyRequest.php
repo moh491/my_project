49da8bc129c6dfe09ea081861e09877a7bd55fd0
loujain diab <loujaindiab551@gmail.com>
2024-03-29 12:28:14 +0300

    add

--- app/Http/Requests/loginCompanyRequest.php
+++ app/Http/Requests/loginCompanyRequest.php
@@ -4,14 +4,14 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class loginCompanyRequest extends FormRequest
+class LoginCompanyRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return true;
+        return false;
     }
 
     /**
@@ -22,8 +22,7 @@ class loginCompanyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email'=>['required'],
-            'password'=>['required','min:10','max:10'],
+            //
         ];
     }
 }
