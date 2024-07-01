d71f8e0ee38a982644406535ccd3a237f2ab1d37
OmarHiger <ohiger2015@gmail.com>
2024-07-01 13:47:38 +0300

    fix error

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -14,18 +14,18 @@ class ProjectSeeder extends Seeder
     public function run(): void
     {
         Project::truncate();
-        Project::create([
-            'title'=>'Online Clothing Store',
-            'description'=>'Built an e-commerce website for a clothing store using React.js and Django. Integrated payment gateway for online transactions, implemented product catalog with filtering and sorting functionalities, and optimized performance for a seamless shopping experience.',
-            'min_budget'=>1000,
-            'max_budget'=>2000,
-            'status'=>4,
-            'project_owner_id'=>1,
-            'field_id'=>3,
-            'worker_type'=>'App\\Models\\Freelancer',
-            'worker_id'=>1,
-        ]);
+//        Project::create([
+//            'title'=>'Online Clothing Store',
+//            'description'=>'Built an e-commerce website for a clothing store using React.js and Django. Integrated payment gateway for online transactions, implemented product catalog with filtering and sorting functionalities, and optimized performance for a seamless shopping experience.',
+//            'min_budget'=>1000,
+//            'max_budget'=>2000,
+//            'status'=>4,
+//            'project_owner_id'=>1,
+//            'field_id'=>3,
+//            'worker_type'=>'App\\Models\\Freelancer',
+//            'worker_id'=>1,
+//        ]);
 
-
+        Project::factory(10);
     }
 }
