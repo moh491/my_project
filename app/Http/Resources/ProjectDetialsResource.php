891df071e550c6d6ff317f11c95cc2f14f9e3baf
moh491 <shm112270@gmail.com>
2024-07-01 10:24:19 +0300

    project done

--- app/Http/Resources/ProjectDetialsResource.php
+++ app/Http/Resources/ProjectDetialsResource.php
@@ -52,6 +52,7 @@ class ProjectDetialsResource extends JsonResource
             'offers_average' => $this->offers()->avg('budget'),
             'offers_number' => $this->offers()->count(),
             'required_skills' => $this->skills->pluck('name'),
+            'ideal_skills' => json_decode($this->ideal_skills)
         ];
 
         if ($this->worker_type === 'App\Models\Freelancer') {
--- app/Services/ProjectService.php
+++ app/Services/ProjectService.php
@@ -24,13 +24,16 @@ class ProjectService
         $skills = $data['skills'];
         unset($data['skills']);
 
+        if (isset($data['ideal_skills'])) {
+            $data['ideal_skills'] = json_encode($data['ideal_skills']);
+        }
+
         return DB::transaction(function () use ($data, $skills) {
 
             $project = Project::create($data);
             if (!empty($skills)) {
                 $project->skills()->attach($skills);
             }
-
             return $project;
         });
 
@@ -47,7 +50,7 @@ class ProjectService
             ->select('id', 'name')
             ->get();
 
-        $skills = Skill::select(DB::raw('name as skill_name'))
+        $skills = Skill::select('id',DB::raw('name as skill_name'))
             ->distinct()
             ->get();
 
