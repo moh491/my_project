d5621fead616293401639e522077fe28cf6aa16a
OmarHiger <ohiger2015@gmail.com>
2024-07-06 02:33:04 +0300

    change baseUrl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
-        $baseUrl = "http://localhost:8000/";
+        $baseUrl = "http://localhost:8000/storage/";
         app()->singleton('baseUrl', function () use ($baseUrl) {
             return $baseUrl;
         });
