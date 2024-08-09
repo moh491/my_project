968fb249c878f3738ecec3b19824983b59f204cb
OmarHiger <ohiger2015@gmail.com>
2024-08-09 15:14:49 +0300

    change in freelancer seeder

--- database/seeders/FreelancerSeeder.php
+++ database/seeders/FreelancerSeeder.php
@@ -23,6 +23,39 @@ class FreelancerSeeder extends Seeder
 
         Freelancer::truncate();
 
+
+        Freelancer::create([
+            'first_name'=>'Loujain',
+            'last_name'=>'Diab',
+            'email'=>'loujaindiab551@gmail.com',
+            'email_verified_at'=>'2023-06-11 19:25:02',
+            'password'=>bcrypt('password'),
+            'position_id'=>1,
+            'about'=>'The description of the freelancer',
+            'location'=>'Damascus, Syria',
+            'profile'=>'freelancer-profile/OIP (1).jpg',
+            'suspended_balance'=>100,
+            'available_balance'=>200,
+            'withdrawal_balance'=>400,
+            'time_zone'=>'EET UTC+3',
+        ]);
+
+        Freelancer::create([
+            'first_name'=>'Sara',
+            'last_name'=>'Mak',
+            'email'=>'sara@gmail.com',
+            'email_verified_at'=>'2023-06-11 19:25:02',
+            'password'=>bcrypt('password'),
+            'position_id'=>2,
+            'about'=>'The description of the freelancer',
+            'location'=>'Damascus, Syria',
+            'profile'=>'freelancer-profile/OIP (1).jpg',
+            'suspended_balance'=>100,
+            'available_balance'=>200,
+            'withdrawal_balance'=>400,
+            'time_zone'=>'EET UTC+3',
+        ]);
+
         $firstNames = [
             'en' => ['John', 'Jane', 'Robert', 'Emily', 'Michael', 'Sarah', 'David', 'Laura', 'James', 'Olivia'],
             'ar' => ['أحمد', 'فاطمة', 'محمد', 'خديجة', 'علي', 'زينب', 'يوسف', 'عائشة', 'عبدالله', 'سلمى']
@@ -156,37 +189,7 @@ class FreelancerSeeder extends Seeder
             ]);
         }
 
-        Freelancer::create([
-            'first_name'=>'Loujain',
-            'last_name'=>'Diab',
-            'email'=>'loujaindiab551@gmail.com',
-            'email_verified_at'=>'2023-06-11 19:25:02',
-            'password'=>bcrypt('password'),
-            'position_id'=>1,
-            'about'=>'The description of the freelancer',
-            'location'=>'Damascus, Syria',
-            'profile'=>'freelancer-profile/OIP (1).jpg',
-            'suspended_balance'=>100,
-            'available_balance'=>200,
-            'withdrawal_balance'=>400,
-            'time_zone'=>'EET UTC+3',
-        ]);
 
-        Freelancer::create([
-            'first_name'=>'Sara',
-            'last_name'=>'Mak',
-            'email'=>'sara@gmail.com',
-            'email_verified_at'=>'2023-06-11 19:25:02',
-            'password'=>bcrypt('password'),
-            'position_id'=>2,
-            'about'=>'The description of the freelancer',
-            'location'=>'Damascus, Syria',
-            'profile'=>'freelancer-profile/OIP (1).jpg',
-            'suspended_balance'=>100,
-            'available_balance'=>200,
-            'withdrawal_balance'=>400,
-            'time_zone'=>'EET UTC+3',
-        ]);
 
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
