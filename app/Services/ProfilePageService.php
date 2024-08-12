55753ce8153a65c34924309c9a887ae740dbd8e7
loujain diab <loujaindiab551@gmail.com>
2024-08-13 01:43:16 +0300

    ..

--- app/Services/ProfilePageService.php
+++ app/Services/ProfilePageService.php
@@ -9,19 +9,35 @@ use Illuminate\Support\Facades\Storage;
 
 class ProfilePageService
 {
-    public function ProfilePage(string $id){
-        $freelancer=Freelancer::find($id);
+    public function ProfilePage(string $id)
+    {
+        $freelancer = Freelancer::find($id);
         return new ProfilePageResource($freelancer);
     }
-    public function updateProfile(string $id,$data){
-        $freelancer=Freelancer::find($id);
+
+    public function updateProfile(string $id, $data)
+    {
+        $freelancer = Freelancer::find($id);
+        if (isset($data['languages'])) {
+            foreach ($data['languages'] as $language) {
+                $freelancer->languages()->updateOrCreate(
+                    ['language' => $language['language']],
+                    ['level' => $language['level']]
+                );
+            }
+        }
+        if (isset($data['skills'])) {
+            $freelancer->skills()->sync($data['skills']);
+        }
         if (isset($data['profile'])) {
             Storage::disk('public')->delete($freelancer->profile);
-            $imageName = $freelancer->id. '-' . $data['profile']->getClientOriginalName();
+            $imageName = $freelancer->id . '-' . $data['profile']->getClientOriginalName();
             $path = $data['profile']->storeAs('freelancer-profile', $imageName, 'public');
             $freelancer->update(['profile' => $path]);
         }
         unset($data['profile']);
+        unset($data['skills']);
+        unset($data['languages']);
         $freelancer->update($data);
     }
 }
