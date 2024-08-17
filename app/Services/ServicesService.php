5a925dedc93a53c441f380b5df62a2b0ffa4b967
OmarHiger <ohiger2015@gmail.com>
2024-08-17 09:09:58 +0300

    fsa

--- app/Services/ServicesService.php
+++ app/Services/ServicesService.php
@@ -35,7 +35,7 @@ class ServicesService
             $user = $model::find($id);
             $services = $user->services()->orderBy('created_at', 'desc')->get();
         } else {
-            return Service::orderBy('created_at', 'desc');
+            return Service::orderBy('created_at', 'desc')->get();
         }
         return ServiceResource::collection($services);
     }
